2fde17758e5b72637df6bbca83eea009f82012f8
xxdyoh <fredysaputro001@gmail.com>
2026-08-11 11:58:50 +0700

    solved limit

--- api/index.js
+++ api/index.js
@@ -7,8 +7,8 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false, limit: '10mb' }));
+app.use(bodyParser.json({ limit: '10mb' }));
 
 const appRoute = require('../src/routes/route');
 app.use('/', appRoute);
